16057c484f118bef1351409fa53d68f98306a2a4
281lasakj-cloud <281lasakj@student.dist113.org>
2026-08-24 12:28:18 +0000

    Answering questions

--- ReferenceLab.java
+++ ReferenceLab.java
@@ -11,14 +11,16 @@ public class ReferenceLab {
 
         // PREDICT BEFORE RUNNING:
         // What will copy.getPosition() return after nemo.setPosition(25)?
-
+        //copy is refered to nemo, so if you change nemo then
+        //copy will also have the same position
         nemo.setPosition(25);
 
         System.out.println("Experiment A - Aliasing");
         System.out.println("nemo position: " + nemo.getPosition());
         System.out.println("copy position: " + copy.getPosition());
 
-
+        //I was correct, when you do copy = nemo, copy now has a
+        //pointer at nemo
         // ==================================================
         // EXPERIMENT B: TWO DIFFERENT OBJECTS
         // ==================================================
@@ -28,12 +30,13 @@ public class ReferenceLab {
 
         // PREDICT BEFORE RUNNING:
         // Will fish1 == fish2 be true or false?
-
+        //False because when you compare objects they are looking
+        //if they are the exact same object or not
         System.out.println();
         System.out.println("Experiment B - Separate Objects");
         System.out.println("fish1 == fish2: " + (fish1 == fish2));
 
-
+        //Was right this was false
         // ==================================================
         // EXPERIMENT C: ARRAY REFERENCES
         // ==================================================
@@ -45,12 +48,15 @@ public class ReferenceLab {
 
         // PREDICT BEFORE RUNNING:
         // What happens to tank[0] if selected is modified?
-
+        //I think tank will change to what selected is
         selected.setPosition(35);
 
         System.out.println();
         System.out.println("Experiment C - Array References");
         System.out.println("selected position: " + selected.getPosition());
         System.out.println("tank[0] position: " + tank[0].getPosition());
+        //tank[0] is the same as selected after it was change which
+        //proves that if you change the variable referring to that position
+        //and tank[0] points to that position it will also change
     }
 }
