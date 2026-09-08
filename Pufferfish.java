73fdea44859f66d749390bc339b4e5a940a95a04
281lasakj-cloud <281lasakj@student.dist113.org>
2026-09-08 03:15:43 +0000

    Adding a new creature pufferfish

--- Pufferfish.java
+++ Pufferfish.java
@@ -1,28 +1,69 @@
 public class Pufferfish extends SeaCreature {
 
-    private String symbol;
-    
 
-    public Pufferfish(String name, int position, int speed, int direction, String symbol) throws InvalidCreatureException {
+    private String symbol;
+    private String puffedSymbol;
+    private int turns;
+
+
+    public Pufferfish(
+            String name,
+            int position,
+            int speed,
+            int direction,
+            String symbol
+    ) throws InvalidCreatureException {
+
+
         super(name, position, speed, direction);
+
+
         this.symbol = symbol;
-        if (name.length() < 3 || name.length() > 20 || position < 0 || speed < 0 || (direction != -1 && direction != 1)) {
-            throw new InvalidCreatureException("Invalid name length for Pufferfish: " + name);
+        this.puffedSymbol = "(@$@)";
+        this.turns = 0;
+
+
+        if (name.length() < 3
+                || name.length() > 20
+                || position < 0
+                || speed < 0
+                || (direction != -1 && direction != 1)) {
+
+
+            throw new InvalidCreatureException(
+                    "Invalid creature parameters for Pufferfish: " + name
+            );
         }
     }
 
+
     @Override
     public void move(int tankWidth) {
+
+
+        turns++;
+
+
         position += speed * direction;
         keepInsideTank(tankWidth);
     }
 
+
     @Override
     public String getSymbol() {
+
+
+        if (turns % 4 == 0) {
+            return puffedSymbol;
+        }
+
+
         return direction >= 0 ? symbol : reverseSymbol(symbol);
     }
+
 
     private String reverseSymbol(String text) {
         return new StringBuilder(text).reverse().toString();
     }
 }
+
